5eb54a7c04d47134a220447c70d6c91901190dfc
Girish Pandit <pandit.girish@gmail.com>
2024-05-30 12:26:53 -0400

    Update app-routing.module.ts

--- angular-dynamic-component-demo_2/src/app/app-routing.module.ts
+++ angular-dynamic-component-demo_2/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { OverviewComponent } from './overview/overview.component';
+import { BuildComponent } from './build/build.component';
+import { ReportComponent } from './report/report.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'overview', component: OverviewComponent },
+  { path: 'build', component: BuildComponent },
+  { path: 'report', component: ReportComponent },
+  { path: '', redirectTo: '/overview', pathMatch: 'full' },
+  { path: '**', redirectTo: '/overview' } // Redirect any other route to overview
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
