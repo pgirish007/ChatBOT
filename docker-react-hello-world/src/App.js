59ce8a1b0df6ae2e1945726b1de574701109f73a
Girish Pandit <pandit.girish@gmail.com>
2024-11-11 11:57:04 -0500

    Add files via upload

---
+++ docker-react-hello-world/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello, World! Welcome to my React App</h1>
+        <p>React deployed with Docker</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
