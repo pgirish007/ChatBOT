507d0953193e43d5621a0f13ff3bb92dc3501296
Girish Pandit <pandit.girish@gmail.com>
2025-03-30 13:04:41 -0400

    Add files via upload

--- nlpTest/maintainContext.py
+++ nlpTest/maintainContext.py
@@ -5,6 +5,7 @@ from nltk.tokenize import word_tokenize
 nltk.download("punkt")
 
 app = Flask(__name__)
+# COMMAND: curl -X POST http://127.0.0.1:5000/chatbot -H "Content-Type: application/json" -d '{"user_id": "123", "message": "I want to build"}'
 
 # Store user conversation state
 user_states = {}
